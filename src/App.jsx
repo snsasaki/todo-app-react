99ad749a653cb6a9c08783328ee05112b881fcde
snsasaki <sn.sasaki@core-tech.jp>
2026-06-16 13:37:32 +0900

    :construction: リスト追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 function App() {
   return (
     <>
-      <h1>タイトル</h1>
+      <h1>Activities</h1>
+      <ul>
+        <li>Todo1</li>
+        <li>Todo2</li>
+        <li>Todo3</li>
+        <li>Todo4</li>
+        <li>Todo5</li>
+      </ul>
     </>
   );
 }
